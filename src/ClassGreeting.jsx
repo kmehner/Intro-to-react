def2bb84048289fdf1c8ca9a5187a758957a41a1
Katelyn Mehner <kmehner@gmail.com>
2024-07-02 16:40:53 -0700

    Adding styles.css into ClassGreeting

--- src/ClassGreeting.jsx
+++ src/ClassGreeting.jsx
@@ -1,13 +1,15 @@
-// src/ClassGreeting.jsx
 
 import React, { Component } from 'react';
+import '../src/styles.css';
 
 class ClassGreeting extends Component {
   //  The render method returns a paragraph element with a welcome message. 
   // The difference from a functional component is that the render method is a class method.
  
     render() {
-        return <p>This is a class-based greeting!</p>;
+
+      // Adding CSS from styles.css -> className instead of Style attribute 
+        return <p className='greeting'>This is a class-based greeting!</p>;
     }
 }
 export default ClassGreeting;
