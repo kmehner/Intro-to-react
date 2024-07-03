5b531a43fb4b00d537ac8f651cd1a71e8460f8c1
Katelyn Mehner <kmehner@gmail.com>
2024-07-03 11:17:22 -0700

    Adding state to functional component, Greeting

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,22 +1,31 @@
 // src/Greeting.jsx
 // A functional component that returns a paragraph element with a welcome message.
 
+// Importing useState from the react library 
+import { useState } from 'react';
 import React from 'react';
 import styles from './Greeting.module.css';
 
 function Greeting() {
+    // Name state variable
+    // setName is a function that updates the name variable
+    // useState is a hook that initializes the name to the default value of 'John Doe'
 
-    // Adding some inline styling 
-    const greetingStyle = {
-    color: "blue",
-    backgroundColor: "lightblue",
-    padding: "10px",
-    fontFamily: "Arial",
-    textAlign: "center"
-    };
+    // <value>, <function to change the value> = useState(<default value>)
+    const [name, setName] = useState('John Doe')
 
-    // Adding the inline styling to the paragraph tag 
-    // return <p style={greetingStyle}>Welcome to the world of React!</p>;
-    return <p className={styles.greeting}>Welcome to the world of React!</p>;
+    const changeName = () => {
+        // Using the setName function to update the name variable to 'Jane Doe'
+        // ["John Doe", setName("John Doe => Jane Doe")]
+        // ["Jane Doe", setName("Jane Doe => x")]
+        setName('Jane Doe')
+    }
+
+    return (
+        <div>
+            <h2 className={styles.greeting}>Welcome, {name}! This is a functional component greeting</h2>
+            <button onClick={changeName}>Change Name</button>
+        </div> 
+    )
 }
 export default Greeting;
