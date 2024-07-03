d5af760a9767448a719e74d61c699faa8acfc187
Katelyn Mehner <kmehner@gmail.com>
2024-07-03 15:07:27 -0700

    listing users for functional component, Greeting

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import ClassGreeting from "./ClassGreeting";
 
 function App() {
   const customMessage = "Welcome to my React app";
+  const customUserMessage =  "Here are our users:";
 
   return (
     <div>
       <h1>Hello, World!</h1>
 
-      <Greeting customMessage={customMessage}/>
-
-      <ClassGreeting customMessage={customMessage} />
+      <Greeting customMessage={customMessage}
+                customUserMessage={customUserMessage} />
+      <ClassGreeting customMessage={customMessage}
+                     customUserMessage={customUserMessage} />
 
     </div>
   );
--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -6,9 +6,8 @@ import styles from './Greeting.module.css';
 
 function Greeting(props) {
     const [name, setName] = useState(props.initialName)
-
-    // Logged in State
     const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [users, setUsers] = useState(["Alice", "Bob", "Charlie", "David", "Eve"]) 
 
     // Event handlers
     const handleLoginClick = () => {
@@ -20,22 +19,28 @@ function Greeting(props) {
         setName(props.initialName) // Reset the name to the initial name
     }
 
-    const changeName = () => {
-        setName('Jane Doe')
-    }
-
     return (
         <div>
             <p className={styles.greeting}>
 								{isLoggedIn ? `Hello, ${name}! Welcome back!` : props.customMessage}
             </p>
 
-            {/* Buttons that are conditionally rendered */}
             {!isLoggedIn ? (
                 <button onClick={handleLoginClick}>Log In</button>
             ) : (
                 <button onClick={handleLogoutClick}>Log Out</button>
             )}
+
+            {/* Listing our users */}
+            <p className={styles.greeting}> 
+                {props.customUserMessage}
+            </p>
+            <ul>
+                {users.map((user, index) => (
+                    <li key={index}>{user}</li>
+                ))}
+            </ul>
+
         </div>
     )
 }
@@ -45,26 +50,3 @@ Greeting.defaultProps = {
 };
 
 export default Greeting;
-
-// function Greeting() {
-//     // Name state variable
-//     // setName is a function that updates the name variable
-//     // useState is a hook that initializes the name to the default value of 'John Doe'
-
-//     // <value>, <function to change the value> = useState(<default value>)
-//     const [name, setName] = useState('John Doe')
-
-//     const changeName = () => {
-//         // Using the setName function to update the name variable to 'Jane Doe'
-//         // ["John Doe", setName("John Doe => Jane Doe")]
-//         // ["Jane Doe", setName("Jane Doe => x")]
-//         setName('Jane Doe')
-//     }
-
-//     return (
-//         <div>
-//             <h2 className={styles.greeting}>Welcome, {name}! This is a functional component greeting</h2>
-//             <button onClick={changeName}>Change Name</button>
-//         </div> 
-//     )
-// }
